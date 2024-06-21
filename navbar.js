0f7ef0ec0c35b0d7287fc9fe66c3e0bdad3d43ad
yousra-azl <yousraamzil@gmail.com>
2024-06-21 12:13:52 +0200

    menue burger terminé

--- navbar.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("openBtn").addEventListener("click", function() {
-    document.getElementById("mySidenav").style.width = "250px";
-});
-
-document.getElementById("closeBtn").addEventListener("click", function() {
-    document.getElementById("mySidenav").style.width = "0";
-});
